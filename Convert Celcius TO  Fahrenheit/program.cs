b91644ee251dc32face8109aef0d8fe6070a0107
Vedant Patil <vedantp.1703@gmail.com>
2026-02-25 12:05:25 +0530

    Add temperature conversion from Celsius to Fahrenheit

---
+++ Convert Celcius TO  Fahrenheit/program.cs
@@ -0,0 +1,18 @@
+using System;
+
+class TemperatureConversion
+{
+    static void Main()
+    {
+        double celsius, fahrenheit;
+
+        Console.Write("Enter temperature in Celsius: ");
+        celsius = Convert.ToDouble(Console.ReadLine());
+
+        fahrenheit = (celsius * 9 / 5) + 32;
+
+        Console.WriteLine("Temperature in Fahrenheit: " + fahrenheit);
+
+        Console.ReadLine();
+    }
+}
