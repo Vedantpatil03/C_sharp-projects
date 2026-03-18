227810aa4958e9a700993b19e077760bc1f0d249
Vedant Patil <vedantp.1703@gmail.com>
2026-03-18 12:01:03 +0530

    Add demonstration of Math class methods in program.cs

---
+++ Demonstrate Math Class/program.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Square root of 25: " + Math.Sqrt(25));
+        Console.WriteLine("Power (2^3): " + Math.Pow(2, 3));
+        Console.WriteLine("Maximum: " + Math.Max(10, 20));
+        Console.WriteLine("Minimum: " + Math.Min(10, 20));
+    }
+}
