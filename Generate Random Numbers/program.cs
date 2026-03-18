4e8888f8869d14529c5fdb60c4b0edb86154a1b9
Vedant Patil <vedantp.1703@gmail.com>
2026-03-18 11:37:35 +0530

    Add program to generate random numbers

---
+++ Generate Random Numbers/program.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random rand = new Random();
+
+        Console.WriteLine("Random Numbers:");
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine(rand.Next(1, 100));
+        }
+    }
+}
